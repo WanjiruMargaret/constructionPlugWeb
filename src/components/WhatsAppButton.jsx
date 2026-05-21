9e12c5d4605d78865c71a539440e010b9226e10a
WanjiruMargaret <maggiesamwells@gmail.com>
2026-05-21 08:26:18 +0300

    final pages

---
+++ src/components/WhatsAppButton.jsx
@@ -0,0 +1,14 @@
+function WhatsAppButton() {
+  return (
+    <a
+      href="https://wa.me/254798305809"
+      target="_blank"
+      rel="noreferrer"
+      className="whatsapp-btn"
+    >
+      💬 Chat With Us
+    </a>
+  );
+}
+
+export default WhatsAppButton;
